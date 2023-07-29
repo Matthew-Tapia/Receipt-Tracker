d3093d8b4546cd94133abc407696585fef331a2b
Matthew-Tapia <matthewtapia8@gmail.com>
2023-07-28 23:17:29 -0700

    added file output to upload api

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,7 +1,8 @@
 export async function POST(req: Request) {
   const data = await req.formData();
+  const file: File | null = data.get("file") as unknown as File;
 
-  console.log(data.get("file"));
+  console.log(file);
 
   return new Response("OK");
 }
