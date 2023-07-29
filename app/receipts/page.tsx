d53da9e9df34a0fe4e0ae9ff9f0f4eb95d7fad85
Bienn Viquiera <biennviquiera@gmail.com>
2023-07-29 13:35:17 -0700

    basic card component implemented. need to create modal view and allow optional fields

--- app/receipts/page.tsx
+++ app/receipts/page.tsx
@@ -1,15 +1,28 @@
 import FileUploader from "@/components/FileUploader";
 import Layout from '@/components/layout'
 import type { ReactElement } from 'react'
+import ItineraryCard from "@/components/ItineraryCard";
 
 
 export default function Home() {
+
+const sampleData = {
+    startDate: '2023-08-01',
+    startTime: '12:30',
+    startLocation: 'SFO',
+    endDate: '2023-08-02',
+    endTime: '12:40 PM',
+    endLocation: 'LAX',
+    cost: '$260',
+    flightNum: 'DL 293',
+    name: 'Flight from SFO to LAX',
+    type: 'flight'
+}
   return (
     <Layout>
       <main>
         <div className="flex justify-center items-center min-h-screen">
-          Receipts go here
-          Fetch existing database entries and display here
+            <ItineraryCard item={sampleData}></ItineraryCard>
         </div>
       </main>
     </Layout>
---
+++ components/ItineraryCard.tsx
@@ -0,0 +1,50 @@
+'use client';
+import React, { useState } from 'react';
+
+interface ItineraryItem {
+    startDate: string;
+    startTime: string;
+    startLocation: string;
+    endDate: string;
+    endTime: string;
+    endLocation: string;
+    cost: string;
+    flightNum: string;
+    name: string;
+    type: string;
+}
+
+interface ItineraryCardProps {
+    item: ItineraryItem;
+}
+
+const ItineraryCard: React.FC<ItineraryCardProps> = ({ item }) => {
+    const [isModalOpen, setIsModalOpen] = useState<Boolean>(false)
+
+    const toggleModal = () => setIsModalOpen(!isModalOpen)
+
+    return (
+        <div className="card bg-white w-320 h-320 rounded-md p-4 cursor-pointer hover:bg-neutral-100" onClick={toggleModal} >
+            Flight from {item.startLocation} to {item.endLocation}
+            <p>
+                <strong>Leaves at </strong> {item.startTime}
+            </p>
+            <p>
+                <strong>Arrives at</strong> {item.endTime}
+            </p>
+            <p>
+                <strong>Cost:</strong> {item.cost}
+            </p>
+            {isModalOpen && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={toggleModal}>&times;</span>
+
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default ItineraryCard
