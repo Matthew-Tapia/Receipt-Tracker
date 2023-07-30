43788a792a0f063898dc3f7418e8c4deb558c80a
Bienn Viquiera <biennviquiera@gmail.com>
2023-07-30 04:05:48 -0700

    localstorage card population implemented

--- app/receipts/page.tsx
+++ app/receipts/page.tsx
@@ -3,34 +3,34 @@ import FileUploader from "@/components/FileUploader";
 import Layout from '@/components/layout'
 import type { ReactElement } from 'react'
 import ItineraryCard from "@/components/ItineraryCard";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-    const sampleData = {
-        startDate: '2023-08-01',
-        startTime: '12:30',
-        startLocation: 'SFO',
-        endDate: '2023-08-02',
-        endTime: '12:40 PM',
-        endLocation: 'LAX',
-        cost: '$260',
-        flightNum: 'DL 293',
-        name: 'Flight from SFO to LAX',
-        type: 'Flight'
-    }
-    const sampleData2 = {
-        startDate: '2023-08-01',
-        startTime: '10:30 AM',
-        startLocation: 'SFO',
-        endDate: '2023-08-02',
-        endTime: '12:40 PM',
-        endLocation: 'JFK',
-        cost: '$333',
-        flightNum: 'DL 293',
-        name: 'Flight from SFO to JFK',
-        type: 'Flight'
-    }
-
+    // const sampleData = {
+    //     startDate: '2023-08-01',
+    //     startTime: '12:30',
+    //     startLocation: 'SFO',
+    //     endDate: '2023-08-02',
+    //     endTime: '12:40 PM',
+    //     endLocation: 'LAX',
+    //     cost: '$260',
+    //     flightNum: 'DL 293',
+    //     name: 'Flight from SFO to LAX',
+    //     type: 'Flight'
+    // }
+    // const sampleData2 = {
+    //     startDate: '2023-08-01',
+    //     startTime: '10:30 AM',
+    //     startLocation: 'SFO',
+    //     endDate: '2023-08-02',
+    //     endTime: '12:40 PM',
+    //     endLocation: 'JFK',
+    //     cost: '$333',
+    //     flightNum: 'DL 293',
+    //     name: 'Flight from SFO to JFK',
+    //     type: 'Flight'
+    // }
+    const [itineraryItems, setItineraryItems] = useState<any[]>([]);
     const [openModalIndex, setOpenModalIndex] = useState<number | null>(null);
 
     const handleOpen = (index: number) => {
@@ -40,9 +40,14 @@ export default function Home() {
             setOpenModalIndex(index);
         }
     };
-
-    const itineraryItems = Array(10).fill(sampleData);
-    itineraryItems.push(sampleData2)
+    useEffect(() => {
+        const data = localStorage.getItem('components');
+        if (data) {
+            setItineraryItems(JSON.parse(data));
+        }
+    }, []);
+    // const itineraryItems = Array(10).fill(sampleData);
+    // itineraryItems.push(sampleData2)
 
     return (
         <Layout>
